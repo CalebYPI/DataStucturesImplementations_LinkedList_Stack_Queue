0acd6e2d6c8d83252432820187e59140d238d9d7
CalebYPI <caleb.ypi@gmail.com>
2022-05-03 12:08:56 +0200

    Updates to queue file

--- src/com/company/queue/MyQueue.java
+++ src/com/company/queue/MyQueue.java
@@ -1,22 +1,29 @@
 package com.company.queue;
 
 public class MyQueue {
+    private int lValue;
+    private int[] data;
 
-    public void enQueue() {
+    public MyQueue() {
+        lValue = -1;
+        data = new int[5];
+    }
 
+    public void enQueue(int d) {
+        //
     }
 
     public void deQueue() {
-
+        //
     }
 
     public static void main(String[] args) {
         MyQueue queue = new MyQueue();
 
-        queue.enQueue();
-        queue.enQueue();
-        queue.enQueue();
-        queue.enQueue();
+        queue.enQueue(6);
+        queue.enQueue(7);
+        queue.enQueue(8);
+        queue.enQueue(9);
 
         queue.deQueue();
     }
