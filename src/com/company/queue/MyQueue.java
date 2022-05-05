5f0ecc4c957c1174d7cede491f6a9c9327e5826c
CalebYPI <caleb.ypi@gmail.com>
2022-05-05 14:44:01 +0200

    Queue class updates

--- src/com/company/queue/MyQueue.java
+++ src/com/company/queue/MyQueue.java
@@ -12,20 +12,24 @@ public class MyQueue {
 
     public void enQueue(int d) {
         data[tail] = d;
-        tail++;
+        tail = (tail + 1)%5;
         size++;
     }
 
     public int deQueue() {
         int v = data[front];
-        front++;
+        front = (front + 1)%5;
         size--;
         return v;
     }
 
     public void display() {
         for (int i = 0; i < size; i++) {
-            System.out.println(data[front+i]);
+            System.out.print(data[(front+i)%5] + " ");
+        }
+        System.out.println();
+        for (int i : data) {
+            System.out.print(i + " ");
         }
     }
 
@@ -33,15 +37,16 @@ public class MyQueue {
         String sfx = " left the queue\n";
         MyQueue queue = new MyQueue();
 
-        queue.enQueue(6);
+        queue.enQueue(60);
         queue.enQueue(7);
-        queue.enQueue(8);
+        queue.enQueue(88);
         queue.enQueue(9);
 
-        queue.display();
-
-        System.out.println("\n" +queue.deQueue() + sfx);
-
+        System.out.print(queue.deQueue() + sfx);
+        System.out.print(queue.deQueue() + sfx);
+        queue.enQueue(11);
+        queue.enQueue(10);
+        queue.enQueue(12);
         queue.display();
     }
 }
